7a0adae0df2071b9dd2e66265d42ffdd399cdf80
Andreas Åkerlöf <andreas@akerlof.se>
2019-02-04 17:06:49 +0100

    Portfolio-layout klar

--- Webbsida/script.js
+++ Webbsida/script.js
@@ -7,11 +7,31 @@ menyknapp.addEventListener('click', toggleMenu);
 menylinks.forEach(link => link.addEventListener('click', toggleMenu));
 
 function toggleMenu() {
-    nav.classList.toggle('show-nav');          
-    menyknapp.innerHTML = (menyknapp.innerHTML == '☰') ? '&#10761;' : '&#9776'; 
+    nav.classList.toggle('show-nav');
+    menyknapp.innerHTML = (menyknapp.innerHTML == '☰') ? '&#10761;' : '&#9776';
 }
 
 // Scrolla tillbaka när man går till en "hash anchor" så att header inte döljer rubriken.
 window.addEventListener('hashchange', scrollUp);
 window.addEventListener('load', scrollUp);
-function scrollUp() { scrollBy(0,-100); }
+function scrollUp() { scrollBy(0, -100); }
+
+// Bildbytare
+const switchers = document.querySelectorAll('.switcher');
+switchers.forEach(switcher => {
+    const switcherLeft = switcher.children[0];
+    const switcherRight = switcher.children[2];
+    switcherLeft.addEventListener('click', switchImage);
+    switcherRight.addEventListener('click', switchImage);
+    switcher.addEventListener('mouseover', showArrows);
+    switcher.addEventListener('mouseout', showArrows);
+});
+
+function switchImage(e) {
+
+}
+
+function showArrows() {
+    this.children[0].classList.toggle('switcher-active-left');
+    this.children[2].classList.toggle('switcher-active-right');
+}
