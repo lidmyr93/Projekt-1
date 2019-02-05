899ded27857da5f1c85d2d8103db52bf2fac402c
Andreas Åkerlöf <andreas@akerlof.se>
2019-02-05 14:07:48 +0100

    Bildvisare färdigställd, innehåll färdigställt!

--- Webbsida/script.js
+++ Webbsida/script.js
@@ -1,4 +1,4 @@
-// Hamburgarmeny
+/* Hamburgarmeny */
 const nav = document.querySelector('nav');
 const menyknapp = document.querySelector('.hamburger');
 const menylinks = document.querySelectorAll('.menu-under a');
@@ -10,13 +10,14 @@ function toggleMenu() {
     nav.classList.toggle('show-nav');
     menyknapp.innerHTML = (menyknapp.innerHTML == '☰') ? '&#10761;' : '&#9776';
 }
+/* Hamburgarmeny end */
 
 // Scrolla tillbaka när man går till en "hash anchor" så att header inte döljer rubriken.
 window.addEventListener('hashchange', scrollUp);
 window.addEventListener('load', scrollUp);
 function scrollUp() { scrollBy(0, -100); }
 
-// Bildbytare
+/* Bildbytare */
 const switchers = document.querySelectorAll('.switcher');
 switchers.forEach(switcher => {
     const switcherLeft = switcher.children[0];
@@ -29,14 +30,36 @@ switchers.forEach(switcher => {
 
 function switchImage() {
     const switcher = this.parentElement.children[1];
-    const offset =  switcher.offsetWidth;
-    console.log(switcher.offsetLeft);
-    (this.classList.contains('switcher-right'))
-        ? switcher.scrollLeft += offset
-        : switcher.scrollLeft -= offset;
+    // imgs hänvisar till switcherns bilder och vänder på ordningen i förhållande till höger/vänster-klick
+    const imgs = (this.classList.contains('switcher-right')) ? switcher.children : [...switcher.children].reverse();
+    // itererar genom switcherns bilder och sätter nästa bild som aktiv
+    let nextImg = false;
+    for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].classList.contains('switcher-active')) {
+            imgs[i].classList.toggle('switcher-active');
+            if (!imgs[i + 1]) i = -1; // om vi nåt slutet av bilderna, gå tillbaka till första.
+            nextImg = !nextImg;
+        }
+        else if (nextImg) {
+            imgs[i].classList.toggle('switcher-active');
+            nextImg = !nextImg;
+        }
+    }
 }
 
 function showArrows() {
     this.children[0].classList.toggle('switcher-active-left');
     this.children[2].classList.toggle('switcher-active-right');
 }
+
+// återställ switcharnas höjd vid storleksändring av fönster
+let resizeEnd;
+window.addEventListener('resize', () => {
+    switchers.forEach(switcher => switcher.style.height = '100%');
+    clearTimeout(resizeEnd);
+    resizeEnd = setTimeout(resetHeight, 250);
+    function resetHeight() {
+        switchers.forEach(switcher => switcher.removeAttribute('style'));
+    }
+});
+/* Bildbytare end */
